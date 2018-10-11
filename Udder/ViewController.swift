5ac1b773a0c8e964970afc9fc36eb4c0d72c6834
timothykruger <aznguitarist@hotmail.com>
2018-10-11 16:29:00 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Timothy Kruger on 10/11/18.
+//  Copyright © 2018 Timothy Kruger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
